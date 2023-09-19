6a8f9be94ae162c5e39a53cb3b951b4c0f47d30e
Aleix <nigeon@gmail.com>
2023-09-19 12:02:26 +0200

    Added Google Provider, removed FB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth, { Account } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
 import { JWT } from "next-auth/jwt";
 
 export const authOptions = {
@@ -10,9 +10,9 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID as string,
-      clientSecret: process.env.FACEBOOK_SECRET as string,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
   jwt: {
