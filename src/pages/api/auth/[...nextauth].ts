bf6f515fde48702a63ad0c71864d65b57c674886
emmdim <emmdim@users.noreply.github.com>
2025-04-09 15:12:41 +0200

    style: Removes unused providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,18 +5,10 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID as string,
-    //   clientSecret: process.env.GITHUB_SECRET as string,
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID as string,
-    //   clientSecret: process.env.FACEBOOK_SECRET as string,
-    // }),
   ],
   jwt: {
     secret: process.env.NEXTAUTH_SECRET,
