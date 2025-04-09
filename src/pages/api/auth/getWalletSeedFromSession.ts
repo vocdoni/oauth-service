dea91f4d4da83568398ca092f4ad111a73e1d302
emmdim <emmdim@users.noreply.github.com>
2025-04-09 15:11:23 +0200

    fix(saas): Improves session checks

--- src/pages/api/auth/getWalletSeedFromSession.ts
+++ src/pages/api/auth/getWalletSeedFromSession.ts
@@ -12,11 +12,26 @@ export default async function handler(
     return res.status(400).json({ error: "No session found" });
   }
 
+  // Check if session.user exists
+  if (!session.user) {
+    return res.status(400).json({ error: "No user found in session" });
+  }
+
+  // Check if email exists
+  if (!session.user.email) {
+    return res.status(400).json({ error: "No email found in user session" });
+  }
+
   const seed = ethers.hashMessage(
     (JSON.stringify(session.user) + process.env.NEXT_AUTH_SEED) as string,
   );
   const wallet = new ethers.Wallet(id(process.env.NEXT_AUTH_SEED as string));
   const signedEmail = await wallet.signMessage(session.user.email);
 
-  res.status(200).json({ seed, email: session.user.email, signedEmail, userName: session.user.name });
+  res.status(200).json({ 
+    seed, 
+    email: session.user.email, 
+    signedEmail, 
+    userName: session.user.name || " " 
+  });
 }
